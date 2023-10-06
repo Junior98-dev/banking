0479acb849641f00073818db80947b71e32c643e
Junior98-dev <kounkouddorianjunior@gmail.com>
2023-10-06 22:06:23 +0200

    améloiration de Http-interceptor

--- src/app/service/http-interceptor/http-interceptor.service.ts
+++ src/app/service/http-interceptor/http-interceptor.service.ts
@@ -11,17 +11,17 @@ export class HttpInterceptorService implements HttpInterceptor{
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrZXNwb2lyQGdtYWlsLmNvbSIsImlhdCI6MTY5NjQ2MjY2MSwiZXhwIjozMzkyOTI1MzIyMzJ9.07dP5PhKnz8dkkO4r4xhMq6dzqvtiEr0ahC_7Mk0Xqw';
-
-    if(token !== undefined && token !== null){
-      // assigner le token
-      
-      const authReq = req.clone({
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token
-        })
-      });
-      return next.handle(authReq);
+    if(!req.url.includes('/authenticate') || !req.url.includes('/register')){
+      const token = localStorage.getItem('token');
+      if(token){
+        // assigner le token
+        const authReq = req.clone({
+          headers: new HttpHeaders({
+            Authorization: 'Bearer ' + token
+          })
+        });
+        return next.handle(authReq);
+      }
     }
     return next.handle(req);
 
