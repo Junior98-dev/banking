8d96ead9d7ac3ef0c78e4b74744f180ddaaccc3b
Junior98-dev <kounkouddorianjunior@gmail.com>
2023-10-07 00:51:33 +0200

    helperService recuperation de userId

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HelperService } from 'src/app/service/helper/helper.service';
+import { ContactDto } from 'src/app/service/models';
+import { ContactControllerService } from 'src/app/service/services';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsComponent {
+export class ContactsComponent  implements OnInit{
+  
+  contacts: Array<ContactDto> = [];
 
+  constructor(
+    private contactService: ContactControllerService,
+    private helperService: HelperService
+    ){}
+
+  ngOnInit(): void {
+    this.contactService.findAllByUserId1({
+      'user-id': this.helperService.userId
+    }).subscribe({
+      next: (data) => {
+        this.contacts = data;
+      }
+    })
+  }
 }
--- src/app/pages/transaction/transaction.component.ts
+++ src/app/pages/transaction/transaction.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HelperService } from 'src/app/service/helper/helper.service';
+import { TransactionDto } from 'src/app/service/models';
+import { TransactionControllerService } from 'src/app/service/services';
 
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.scss']
 })
-export class TransactionComponent {
+export class TransactionComponent implements OnInit{
 
+  transactions: Array<TransactionDto> = [];
+
+  constructor(
+    private transactionService: TransactionControllerService,
+    private helper: HelperService
+    ){}
+
+  ngOnInit(): void {
+      this.transactionService.findAllByUserId({
+        'user-id': this.helper.userId
+      }).subscribe({
+        next: (data) => {
+          this.transactions = data;
+        }
+      })
+  }
 }
