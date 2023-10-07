1737718bcd25138ab6226a8cf5daa9ab8bc13ab6
Junior98-dev <kounkouddorianjunior@gmail.com>
2023-10-07 23:38:49 +0200

    pages new transaction terminée

--- src/app/admin/manager-users/manager-users.component.ts
+++ src/app/admin/manager-users/manager-users.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-manager-users',
   templateUrl: './manager-users.component.html',
   styleUrls: ['./manager-users.component.scss']
 })
-export class ManagerUsersComponent {
+export class ManagerUsersComponent implements OnInit{
+  
+  constructor(){}
+  
+  ngOnInit(): void {
+    
+  }
 
 }
--- src/app/pages/new-transaction/new-transaction.component.ts
+++ src/app/pages/new-transaction/new-transaction.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HelperService } from 'src/app/service/helper/helper.service';
+import { ContactDto, TransactionDto } from 'src/app/service/models';
+import { ContactControllerService, StatisticsControllerService, TransactionControllerService } from 'src/app/service/services';
 
 @Component({
   selector: 'app-new-transaction',
   templateUrl: './new-transaction.component.html',
   styleUrls: ['./new-transaction.component.scss']
 })
-export class NewTransactionComponent {
+export class NewTransactionComponent implements OnInit{
+  
+  transaction: TransactionDto = {};
+  contacts: Array<ContactDto> = [];
+  accountBalance = 0;
+  errorMessages: Array<string> = [];
+  
+  constructor(
+    private statisticService: StatisticsControllerService,
+    private contactService: ContactControllerService,
+    private transactionService: TransactionControllerService,
+    private helperService: HelperService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.findAllContactByUser();
+    this.statisticService.getAccountBalance({
+      'user-id': this.helperService.userId
+    }).subscribe({
+      next: (data) => {
+        this.accountBalance = data;
+      }
+    })
+  }
+
+  private findAllContactByUser(){
+    this.contactService.findAllByUserId1({
+      'user-id': this.helperService.userId
+    }).subscribe({
+      next: (data) => {
+        this.contacts = data;
+      }
+    })
+  }
+
+  async cancel(){
+    await this.router.navigate(['user','my-transaction']);
+  }
+
+  save(){
+    this.errorMessages = [];
+    this.transaction.userId = this.helperService.userId;
+    this.transactionService.save1({
+      body: this.transaction
+    }).subscribe({
+      next: async () => {
+        await this.router.navigate(['user','my-transaction'])
+      },
+      error: (err) => {
+        this.errorMessages = err.error.validationError;
+      }
+    });
+  }
+
 
 }
