3cf773da0dd33c51b48b5a1bd06539adfffb01d4
Junior98-dev <kounkouddorianjunior@gmail.com>
2023-10-08 01:44:39 +0200

    bouton utilisateur inactive ok

--- src/app/admin/manager-users/manager-users.component.ts
+++ src/app/admin/manager-users/manager-users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDto } from 'src/app/service/models';
+import { UserControllerService } from 'src/app/service/services';
 
 @Component({
   selector: 'app-manager-users',
@@ -7,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerUsersComponent implements OnInit{
   
-  constructor(){}
+  customers: Array<UserDto> = [];
+  showInactiveUserOnly = false;
   
+  constructor(
+    private userService: UserControllerService
+
+  ){}
+
   ngOnInit(): void {
-    
+    this.findAllCustomers();
+  }
+  private findAllCustomers(){
+    this.userService.findAll()
+    .subscribe({
+      next: (value) => {
+        this.customers = value;
+      }
+    });
+  }
+
+  filterCustomers(){
+    if(this.showInactiveUserOnly){
+      this.customers = this.customers.filter((c) => !c.active);
+    }else{
+      this.findAllCustomers();
+    }
   }
 
 }
